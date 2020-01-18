af9a32371d5a5b98600b03731ae7487a1724d4e5
rizredvlo7zer0 <50101349+rizredvlo7zer0@users.noreply.github.com>
2020-01-18 02:51:38 +0000

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,68 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-image: url("https://1.bp.blogspot.com/-eLTvVd1yQSs/XiFUjvHwyyI/AAAAAAAAAR8/OYWSm4-IpM0MrH_aaTBoe5gKP6eDBoitACLcBGAsYHQ/s1600/20200117_132912.jpg");
+  background-repeat: no-repeat;
+  background-position: right top;
+  margin-right: 200px;
+  background-attachment: fixed;
+  background-size: 100% ;
+  background-position:top center;
+}
+</style>
+</head>
+<body>
+
+<style>body{cursor: url(), progress !important; repeat center;color:lime;} .kotak{text-align:center;background:transparant;border:solid lime
+2px;border-radius:10px;position:fixed;top:2px;left:2px;left:1px;right:1px;}.kotak1{text-align:center;background:transparant;border:solid lime 2px;border-radius:10px;position:fixed;bottom:2px;left:1px;right:1px;}</style><br/><br/>
+
+
+<div class="kotak"><font color="green">••••• Zona Red •••••</font></div><div class="kotak1"><blink>|"Dev By Rizred Vlo 7zer0✬"|</blink></div></center>
+
+
+
+
+<center>
+
+<style>
+.button {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+  cursor: pointer;
+  repeat center;
+}
+
+.button {
+  background-color: transparent; 
+  color: white; 
+  border: 2px solid #4CAF50;
+  border-radius: 12px;
+}
+a {
+  color: white;
+}
+
+.button:hover {
+  background-color: #;
+  color: red;
+}
+
+
+
+</style>
+</head>
+<body>
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -19,33 +82,26 @@
   </head>
   <body>
     <center>
-    <div class="paper container">
-      <h1>Bicit Giniritir</h1>
+      <h1><font color="red">Bacot Generator</h1>
       <div class="row">
         <div class="col-md">
-          <label for="paperInputs1">Misikin Tiks</label>
+          <label for="paperInputs1"><font color="white">Masukan Text</label>
           <input class="btn-block" type="text" placeholder="Misikin Tiks" id="paperInputs1">
         </div>
       </div>
       <div class="row">
         <div class="col-md">
-          <label for="paperInputs1">Hisil Tiks</label>
+          <label for="paperInputs1">Hasil Text</label>
           <input class="btn-block" type="text" placeholder="Hisil Tiks" id="paperInputs2" readonly>
           
         </div>
       </div>
       <div class="row">
         <div class="col-md">
-          <button id="ubah">Ibih Tilisin</button>
-          <button id="copy">Cipy Tilisin</button>
+          <button id="ubah">Ubah Tulisan</button>
+          <button id="copy">Copy Tulisan</button>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md">
-          <h3>Created By : Rzr</h3>
-        </div>
-      </div>
-    </div>
   </center>
     
     <!-- Optional JavaScript -->
@@ -56,4 +112,89 @@
     <script src="https://unpkg.com/papercss@1.6.1/dist/paper.css"></script>
     <script src="bacot.js"></script>
   </body>
+</html>
+
+<br><br><br><br><br><br>
+
+<head>
+<style>
+body {margin:0;}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+</style>
+</head>
+<body>
+
+<head>
+<style>
+body {margin:0;}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #4CAF50;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><b><font color="red">@copyright by Rizredvlo</b></font></li>
+</ul>
+
+</body>
 </html>
